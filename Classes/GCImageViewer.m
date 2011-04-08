8a1b51b92d41b3bf310fb777dc7a3a2833bb4e43
stephandev <stephandeveloper@gmail.com>
2011-04-08 21:45:44 +0200

    Changed the Licence query in the newly added classes

--- Classes/GCImageViewer.m
+++ Classes/GCImageViewer.m
@@ -1,10 +1,27 @@
 //
-//  GCImageViewer.m
-//  ImageViewerTest
+//  GCImageviewer.m
+//  Apfeltalk Magazin
 //
-//  Created by Stefan Kofler on 13.12.10.
-//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//	Apfeltalk Magazin -- An iPhone Application for the site http://apfeltalk.de
+//	Copyright (C) 2009	Stephan König (stephankoenig at me dot com), Stefan Kofler
+//						Alexander von Below, Andreas Rami, Michael Fenske, Laurids Düllmann, Jesper Frommherz (Graphics),
+//						Patrick Rollbis (Graphics),
+//						
+//	This program is free software; you can redistribute it and/or
+//	modify it under the terms of the GNU General Public License
+//	as published by the Free Software Foundation; either version 2
+//	of the License, or (at your option) any later version.
 //
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with this program; if not, write to the Free Software
+//	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.//
+//
+
 
 #import "GCImageViewer.h"
 
