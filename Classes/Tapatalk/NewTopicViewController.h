f4e69ca48c1833fbf31d42c3bc3f44464dd6877f
Manuel Burghard <manuel.burghard@googlemail.com>
2011-05-14 21:08:52 +0200

    NewTopicViewController

---
+++ Classes/Tapatalk/NewTopicViewController.h
@@ -0,0 +1,25 @@
+//
+//  NewTopicViewController.h
+//  Apfeltalk Magazin
+//
+//  Created by Manuel Burghard on 14.05.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AnswerViewController.h"
+#import "SubForum.h"
+
+@interface NewTopicViewController : AnswerViewController <NSXMLParserDelegate> {
+    SubForum *forum;
+    NSMutableData *receivedData;
+    UITextField *topicField;
+}
+
+@property (retain) SubForum *forum;
+@property (retain) NSMutableData *receivedData;
+@property (retain) UITextField *topicField;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil forum:(SubForum *)aForum;
+
+@end
