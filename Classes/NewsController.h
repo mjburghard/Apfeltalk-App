ba32965b9ac13f2569ad2c8dfc472ebb9dc72e4d
Manuel Burghard <manuel.burghard@googlemail.com>
2011-06-05 19:14:20 +0200

    Superclass now handles rotations. Removed not needed code.

--- Classes/NewsController.h
+++ Classes/NewsController.h
@@ -24,6 +24,7 @@
 
 #import <UIKit/UIKit.h>
 #import "RootViewController.h"
+#import "Story.h"
 
 @interface NewsController : RootViewController <UIAccelerometerDelegate, UIActionSheetDelegate> {
 	@private
