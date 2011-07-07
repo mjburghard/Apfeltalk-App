6b456e9155d8b46a4e09dc6967a66cdb09898242
stephandev <stephandeveloper@gmail.com>
2011-07-07 20:13:40 +0200

    Tabbar is now hidden when watching a thread in the forum

--- Classes/Tapatalk/DetailThreadController.m
+++ Classes/Tapatalk/DetailThreadController.m
@@ -528,6 +528,7 @@ const CGFloat kDefaultRowHeight = 44.0;
     [self.navigationController pushViewController:answerViewController animated:YES];
     answerViewController.textView.text = answerCell.textView.text;
     answerCell.textView.text = @"";
+    [self.navigationController pushViewController:answerViewController animated:YES];
     [answerViewController release];
 }
 
