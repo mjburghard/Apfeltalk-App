d60e53bee4e0728b18f59b328ed3bfdb4a5357f0
Manuel Burghard <manuel.burghard@googlemail.com>
2011-08-14 21:25:42 +0200

    New controller to show subscriptions.

---
+++ Classes/Tapatalk/SubscriptionsViewController.h
@@ -0,0 +1,16 @@
+//
+//  SubscriptionsViewController.h
+//  Apfeltalk Magazin
+//
+//  Created by Manuel Burghard on 14.08.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "SubForumController.h"
+
+@interface SubscriptionsViewController : SubForumController {
+    
+}
+
+@end
