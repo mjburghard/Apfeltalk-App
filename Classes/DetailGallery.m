39c1c00f412dff13194a49e924fb08f0663b67dd
stephandev <stephandeveloper@gmail.com>
2011-08-23 23:36:59 +0200

    Tried to localize some buttons

--- Classes/DetailGallery.m
+++ Classes/DetailGallery.m
@@ -41,9 +41,9 @@
 
 - (void)showFullscreen {
     NSString *str = [[self story] summary];
-	
+    
     NSString *thumbLink = extractTextFromHTMLForQuery(str, @"//img[attribute::title]/attribute::src");
-	
+    
     if ([thumbLink length] == NSNotFound) {
 		UIAlertView * errorAlert = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Keine URL", @"")
                                                               message:NSLocalizedString (@"Es konnte keine URL für das Bild gefunden werden", @"")
@@ -74,7 +74,7 @@
     thumbLink = [thumbLink stringByReplacingOccurrencesOfString:@"/thumbs" withString:@""];
     
     NSURL *loadURL = [[request URL] retain]; // retain the loadURL for use
-   
+    
     if ([[loadURL absoluteString] isEqualToString:thumbLink]) {
         if (![webView isLoading]) {
             [self showFullscreen];
@@ -92,17 +92,17 @@
 	NSScanner *scanner;
 	NSString *text = nil;
 	NSString *tag = nil;
-	
+    
 	//set up the scanner
 	scanner = [NSScanner scannerWithString:data];
-	
+    
 	while([scanner isAtEnd] == NO) {
 		//find start of tag
 		[scanner scanUpToString:@"<" intoString:NULL];
-		
+        
 		//find end of tag
 		[scanner scanUpToString:@">" intoString:&text];
-		
+        
 		//get the name of the tag
 		if([text rangeOfString:@"</"].location != NSNotFound)
 			tag = [text substringFromIndex:2]; //remove </
@@ -113,13 +113,13 @@
 				//remove text after a space
 				tag = [tag substringToIndex:[tag rangeOfString:@" "].location];
 		}
-		
+        
 		//if not a valid tag, replace the tag with a space
 		if([valid_tags containsObject:tag] == NO)
 			data = [data stringByReplacingOccurrencesOfString:
 					[NSString stringWithFormat:@"%@>", text] withString:@""];
 	}
-	
+    
 	//return the cleaned up data
 	return data;
 }
@@ -160,17 +160,17 @@ void endElement (void *userData, const xmlChar *name) {
 	memset(&saxInfo, 0, sizeof(saxInfo));
 	saxInfo.characters = &characters;
 	saxInfo.endElement = &endElement;
-	
+    
 	// :below:20090919 The following will strip extract the pure text and breaks from the HTML
-	
+    
 	// The big problem is that we don't have really structured input here, so all of this remains pretty brute force
-	
+    
 	// Input is:
 	//	<a href="http://www.apfeltalk.de/gallery/showphoto.php?photo=15992" target="_blank"><img title="Screenshot_2009-09-17_at_16_31_09.png" border="0" src="http://www.apfeltalk.de/gallery/data/500/thumbs/Screenshot_2009-09-17_at_16_31_09.png" alt="Screenshot_2009-09-17_at_16_31_09.png" /></a><br /><br />von: GunBound<br /><br />Beschreibung: Nein, eigentlich ist ja alles klar\u0085<br /><br />6 Kommentare<img width='1' height='1' src='http://rss.feedsportal.com/c/837/f/10954/s/6302265/mf.gif' border='0'/><div class='mf-viral'><table border='0'><tr><td valign='middle'><a href="http://res.feedsportal.com/viral/sendemail2_de.html?title=H\u00f8\u00f8\u00f8\u00f8\u00f8?&link=http://www.apfeltalk.de/gallery/showphoto.php?photo=15992" target="_blank"><img src="http://rss.feedsportal.com/images/emailthis2.gif" border="0" /></a></td><td valign='middle'><a href="http://res.feedsportal.com/viral/bookmark_de.cfm?title=H\u00f8\u00f8\u00f8\u00f8\u00f8?&link=http://www.apfeltalk.de/gallery/showphoto.php?photo=15992" target="_blank"><img src="http://rss.feedsportal.com/images/bookmark.gif" border="0" /></a></td></tr></table></div><br/><br/><a href="http://da.feedsportal.com/r/50216880195/u/82/f/10954/c/837/s/103817829/a2.htm"><img src="http://da.feedsportal.com/r/50216880195/u/82/f/10954/c/837/s/103817829/a2.img" border="0"/></a>
 	// Output should be:
 	// <br /><br />von: GunBound<br /><br />Beschreibung: Nein, eigentlich ist ja alles klar\u0085<br /><br />6 Kommentare<br/><br/>
 	// TODO: Add a unit test for this
-	
+    
 	// We could try if seperating it by <br/> Tags works...
     
 	xmlChar *xmlCharString = (xmlChar*)[[[self story] summary] cStringUsingEncoding:NSUTF8StringEncoding];
@@ -178,7 +178,7 @@ void endElement (void *userData, const xmlChar *name) {
 												 "utf-8", 
 												 &saxInfo, 
 												 cleanedString)	;
-		
+    
 	free (htmlDoc);
 	//[elementString release];
 	NSString *str = [[[self story] thumbnailLink] stringByReplacingOccurrencesOfString:@"/thumbs" withString:@""];
@@ -231,24 +231,24 @@ void endElement (void *userData, const xmlChar *name) {
 {
     [super speichern:sender];
 	Apfeltalk_MagazinAppDelegate *appDelegate = (Apfeltalk_MagazinAppDelegate *)[[UIApplication sharedApplication] delegate];
-
+    
 	// :below:20090920 This is only to placate the analyzer
 	myMenu = [[UIActionSheet alloc]
-							 initWithTitle: nil
-							 delegate:self
-							 cancelButtonTitle:NSLocalizedStringFromTable(@"Cancel", @"ATLocalizable", @"")
-							 destructiveButtonTitle:nil
-							 otherButtonTitles:@"Per Mail versenden", @"Bild speichern", @"Zeige Bild",nil];
-
+              initWithTitle: nil
+              delegate:self
+              cancelButtonTitle:NSLocalizedStringFromTable(@"Cancel", @"ATLocalizable", @"")
+              destructiveButtonTitle:nil
+              otherButtonTitles:@"Per Mail versenden", @"Bild speichern", @"Zeige Bild",nil];
+    
     [myMenu showFromTabBar:[[appDelegate tabBarController] tabBar]];
 }
 
 - (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIdx
 {
 	NSString *str = [[self story] summary];
-	
+    
     NSString *thumbLink = extractTextFromHTMLForQuery(str, @"//img[attribute::title]/attribute::src");
-	
+    
     if ([thumbLink length] == NSNotFound) {
 		UIAlertView * errorAlert = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Keine URL", @"")
                                                               message:NSLocalizedString (@"Es konnte keine URL für das Bild gefunden werden", @"")
@@ -258,7 +258,7 @@ void endElement (void *userData, const xmlChar *name) {
 		[errorAlert release];
         return;
     }
-
+    
     NSString *imageLink = [thumbLink stringByReplacingOccurrencesOfString:@"/thumbs" withString:@""];
     
     switch (buttonIdx) {
@@ -286,7 +286,7 @@ void endElement (void *userData, const xmlChar *name) {
             [self createMailComposerWithThumbnailLink:imageLink];
         }
     }
-	
+    
 	// :below:20090920 This is only to placate the analyzer
 	if (actionSheet == myMenu) {
 		[myMenu release];
@@ -298,7 +298,7 @@ void endElement (void *userData, const xmlChar *name) {
     
 	MFMailComposeViewController *controller = [[MFMailComposeViewController alloc] init];
 	controller.mailComposeDelegate = self;
-	
+    
     NSString *imageLink = [thumbnailLink stringByReplacingOccurrencesOfString:@"/thumbs" withString:@"/medium"];
     
 	// adde image as attachment
@@ -307,7 +307,7 @@ void endElement (void *userData, const xmlChar *name) {
         imageLink = [thumbnailLink stringByReplacingOccurrencesOfString:@"/thumbs" withString:@""];
         image = [UIImage imageWithData: [NSData dataWithContentsOfURL: [NSURL URLWithString:imageLink]]];
     }
-      
+    
     if (image == nil) {
         UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:NSLocalizedString (@"Foto konnte nicht geladen werden", @"")
                                                             message:NSLocalizedString (@"Das Foto konnte in keiner Auflösung geladen werden", @"")
@@ -321,7 +321,7 @@ void endElement (void *userData, const xmlChar *name) {
     
 	NSData *imageData = UIImageJPEGRepresentation(image, 1);
 	[controller addAttachmentData:imageData mimeType:@"image/jpg" fileName:@"attachment.jpg"];
-	
+    
 	[controller setSubject:[story title]];
 	[self presentModalViewController:controller animated:YES];
 	[controller release];
